b04af188408319d13566147e4098d0bcce4b1199
TMurteira <tiagomurteira2000@gmail.com>
2020-05-27 17:41:37 +0100

    Criação do branch Depositos juntamente com o seu Controller e Views. Criação de uma navbar de Depositos.

--- BetOven/Models/Depositos.cs
+++ BetOven/Models/Depositos.cs
@@ -7,17 +7,46 @@ using System.Threading.Tasks;
 
 namespace BetOven.Models
 {
+    /// <summary>
+    /// Representa os dados de um 'Depósito'
+    /// </summary>
     public class Depositos
     {
+        /// <summary>
+        /// Identificador do Depósito, será PK na tabela Depositos
+        /// </summary>
         [Key]
+        [Display(Name = "Número do Depósito")]
         public int NDeposito { get; set; }
+
+        /// <summary>
+        /// Montante a ser depositado
+        /// </summary>
+        //[RegularExpression("[0-9]+[€]")]
         public double Montante { get; set; }
+
+        /// <summary>
+        /// Data em que o depósito foi efetuado
+        /// </summary>
+        [DataType(DataType.Date)]
         public DateTime Data { get; set; }
+
+        /// <summary>
+        /// Formato em que o pagamento do deposito foi efetuado
+        /// Exs.: Paypal, Multibanco, Cartão Virtual (MB WAY), Cartão de Crédito, etc...
+        /// </summary>
+        [Display(Name = "Formato de Pagamento")]
         public string Formato_pagamento { get; set; }
+
+        /// <summary>
+        /// Hipótese de o depósito ter sido feito pelo utilizador ou pelo sistema
+        /// </summary>
+        [Display(Name = "Origem do Depósito")]
         public string Origem_deposito { get; set; }
 
         //FK para Users
         [ForeignKey("User")]
+        [Display(Name = "Utilizador")]
         public int UserFK { get; set; }
         public Users User { get; set; }
     }
