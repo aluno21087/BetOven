b77b18759ad05fb58b83b14e5fc507dc312713fb
TMurteira <tiagomurteira2000@gmail.com>
2020-07-10 22:04:11 +0100

    Commit para correção de um bug na Migration

---
+++ BetOven/BetOven/Data/Migrations/20200710210026_teste.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BetOven.Data.Migrations
+{
+    public partial class teste : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FotografiaA",
+                table: "Jogos");
+
+            migrationBuilder.DropColumn(
+                name: "FotografiaB",
+                table: "Jogos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FotoA",
+                table: "Jogos",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FotoB",
+                table: "Jogos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FotoA",
+                table: "Jogos");
+
+            migrationBuilder.DropColumn(
+                name: "FotoB",
+                table: "Jogos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FotografiaA",
+                table: "Jogos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FotografiaB",
+                table: "Jogos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
